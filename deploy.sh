d84fc73543d26fbbd014c5642d54f72cb8d26abe
AlzyWelzy <welzyalzy@gmail.com>
2023-12-01 16:58:39 +0530

    working

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ cd /root/repo-auto/your-node-app
 # Pull the latest changes from the GitHub repository
 git pull origin main
 
-# Install or update dependencies (assuming you have a package.json file)
-npm install
 
 # Restart your Node.js application using pm2 (adjust the script based on your actual start command)
 echo "Your project is deployed"
