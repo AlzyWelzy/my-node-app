f9b0da939323f5c218f46181e7980a3e48f68e5d
AlzyWelzy <welzyalzy@gmail.com>
2023-12-01 16:48:14 +0530

    .

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the repository directory
+cd /root/repo-auto/your-node-app
+
+# Pull the latest changes from the GitHub repository
+git pull origin master
+
+# Install or update dependencies (assuming you have a package.json file)
+npm install
+
+# Restart your Node.js application using pm2 (adjust the script based on your actual start command)
+pm2 restart index.js
